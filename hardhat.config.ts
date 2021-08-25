8c5f09a1436cd4c30a078bd7f7e55ba8eddc4663
maroonstar <744428333@qq.com>
2021-08-25 16:20:52 +0800

    hardthat task test a part, not all testfile

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,29 +1,42 @@
-import { task } from "hardhat/config";
-import "@nomiclabs/hardhat-waffle";
+import { subtask, task } from 'hardhat/config';
+import '@nomiclabs/hardhat-waffle';
+import { TASK_TEST } from 'hardhat/builtin-tasks/task-names';
+import * as path from 'path';
+import glob from 'glob';
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (args, hre) => {
-  const accounts = await hre.ethers.getSigners();
+async function getTestFiles(partName: string) {
+  return new Promise((resolve, reject) => {
+    glob(path.join(partName, 'test/*.test.ts'), function (err, files) {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(files);
+    });
+  });
+}
 
-  for (const account of accounts) {
-    console.log(await account.address);
-  }
-});
+// 针对章节测试
+task('testpart', 'test a part')
+  .addOptionalParam('name')
+  .setAction(async (args, hre) => {
+    const files = await getTestFiles(args.name);
+    console.log('testFiles: ', files);
 
-task("tsetBasic", "Prints the list of accounts", async (args, hre) => {
-  
-});
+    await hre.run(TASK_TEST, {
+      testFiles: files,
+    });
+  });
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
 export default {
-  solidity: "0.8.7",
+  solidity: '0.8.7',
   paths: {
-    sources: "./*/contracts",
-    tests: "./*/test",
-    cache: "./cache",
-    artifacts: "./artifacts"
+    sources: './*/contracts',
+    tests: './*/test',
+    cache: './cache',
+    artifacts: './artifacts',
   },
 };
