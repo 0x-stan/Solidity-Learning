7deb6690572218351c681dedd6a26414b9dfe3bf
maroonstar <744428333@qq.com>
2021-09-11 12:58:11 +0800

    blindAuction

--- Basics/contracts/BlindAuction.sol
+++ Basics/contracts/BlindAuction.sol
@@ -17,10 +17,24 @@ contract BlindAuction {
     address public highestBidder;
     uint256 public highestBid;
 
+    // Allowed withdrawals of previous bids
     mapping(address => uint256) pendingReturns;
 
     event AuctionEnded(address winner, uint256 highestBid);
 
+    // Errors that describe failures.
+
+    /// The function has been called too early.
+    /// Try again at `time`.
+    error TooEarly(uint256 time);
+    error TooLate(uint256 time);
+    /// The function auctionEnd has already been called.
+    error AuctionEndAlreadyCalled();
+
+    // Modifiers are a convenient way to validate inputs to
+    // functions. `onlyBefore` is applied to `bid` below:
+    // The new function body is the modifier's body where
+    // `_` is replaced by the old function body.
     modifier onlyBefore(uint256 _time) {
         require(block.timestamp < _time);
         _;
@@ -40,9 +54,94 @@ contract BlindAuction {
         revealEnd = biddingEnd + _revealTime;
     }
 
+    /// Place a blinded bid with `blindedBid` =
+    /// keccak256(abi.encodePacked(value, fake, secret)).
+    /// The sent ether is only refunded if the bid is correctly
+    /// revealed in the revealing phase. The bid is valid if the
+    /// ether sent together with the bid is at least "value" and
+    /// "fake" is not true. Setting "fake" to true and sending
+    /// not the exact amount are ways to hide the real bid but
+    /// still make the required deposit. The same address can
+    /// place multiple bids.
     function bid(bytes32 _blindedBid) public payable onlyBefore(biddingEnd) {
         bids[msg.sender].push(
             Bid({blindedBid: _blindedBid, deposit: msg.value})
         );
+    }
+
+    /// Reveal your blinded bids. You will get a refund for all
+    /// correctly blinded invalid bids and for all bids except for
+    /// the totally highest.
+    function reveal(
+        uint256[] _values,
+        bool[] _fake,
+        bytes32[] _secret
+    ) external onlyAfter(biddingEnd) onlyBefore(revealEnd) {
+        // 取length长度，拿到memory中缓存，避免三次读取storage
+        uint256 length = bids[msg.sender].length;
+        require(_values.length == length);
+        require(_fake.length == length);
+        require(_secret.length == length);
+
+        uint256 refund;
+        for (uint256 i = 0; i < length; i++) {
+            Bid storage bid = bids[msg.sender][i];
+            (uint256 value, bool fake, bytes32 secrect) = (
+                _values[i],
+                _fake[i],
+                _secret[i]
+            );
+
+            if (bid.blindedBid != keccak256(value, fake, secrect)) {
+                continue;
+            }
+            refund += bid.deposit;
+            if (!fake && bid.deposit >= value) {
+                if (placeBid(msg.sender, value)) {
+                    refund -= value;
+                }
+            }
+            bid.blindedBid = bytes32(0);
+        }
+        msg.sender.transfer(refund);
+    }
+
+    function withdraw() external {
+        uint256 amount = pendingReturns[msg.sender];
+        if (amount > 0) {
+            // It is important to set this to zero because the recipient
+            // can call this function again as part of the receiving call
+            // before `transfer` returns (see the remark above about
+            // conditions -> effects -> interaction).
+            pendingReturns[msg.sender] = 0;
+
+            payable(msg.sender).transfer(amount);
+        }
+    }
+
+    function auctionEnd() public onlyAfter(revealEnd) {
+        require(!ended);
+        emit AuctionEnded(highestBidder, highestBid);
+        ended = true;
+        beneficiary.transfer(highestBid);
+    }
+
+    // This is an "internal" function which means that it
+    // can only be called from the contract itself (or from
+    // derived contracts).
+    function placeBid(address bidder, uint256 value)
+        internal
+        returns (bool success)
+    {
+        if (value <= highestBid) {
+            return false;
+        }
+        if (highestBidder != address(0)) {
+            // Refund the previously highest bidder.
+            pendingReturns[highestBidder] += highestBid;
+        }
+        highestBid = value;
+        highestBidder = bidder;
+        return true;
     }
 }
---
+++ Basics/test/BlindAuction.test.ts
@@ -0,0 +1,66 @@
+import '@typechain/hardhat';
+import '@nomiclabs/hardhat-ethers';
+
+import { ethers } from 'hardhat';
+import { Signer, Contract, utils, BigNumber } from 'ethers';
+import chai from 'chai';
+import { solidity } from 'ethereum-waffle';
+
+import {
+  passBlocks,
+  getBlcokTimestamp,
+  GAS_PRICE,
+  extendDecimals,
+} from '../../utils';
+
+chai.use(solidity);
+
+const { expect } = chai;
+
+const one_eth = extendDecimals(1);
+describe('BlindAuction', function () {
+  let owner: Signer, user1: Signer, user2: Signer;
+  let ownerAddress: string, user1Address: string, user2Address: string;
+  let blindAuction: Contract;
+
+  let beginTime: number;
+  let biddingTime: number;
+
+  async function initializeProccess(
+    _biddingTime: number,
+    _onwerAddress: string
+  ) {
+    biddingTime = _biddingTime;
+    const BlindAuction = await ethers.getContractFactory('BlindAuction');
+    blindAuction = await BlindAuction.deploy(biddingTime, ownerAddress);
+
+    // get block.timestamp when contract deployed
+    beginTime = await getBlcokTimestamp();
+  }
+
+  beforeEach(async function () {
+    [owner, user1, user2] = await ethers.getSigners();
+    ownerAddress = await owner.getAddress();
+    user1Address = await user1.getAddress();
+    user2Address = await user2.getAddress();
+  });
+
+  describe('contructor()', async function () {
+    it('should BlindAuction initialize correctly.', async function () {
+      await initializeProccess(60, ownerAddress);
+
+      expect(await blindAuction.auctionEndTime()).to.equals(
+        beginTime + biddingTime
+      );
+      expect(await blindAuction.beneficiary()).to.equals(ownerAddress);
+    });
+  });
+
+  describe('bid()', async function () {
+    
+  });
+
+  describe('auctionEnd()', async function () {
+    
+  });
+});
